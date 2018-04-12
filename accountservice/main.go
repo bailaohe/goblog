53a6c42eeb953076c833031c3156685a679b5960
bailaohe <bailaohe@gmail.com>
2018-04-12 20:45:10 +0800

    updated

--- accountservice/main.go
+++ accountservice/main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/bailaohe/goblog/accountservice/service"
+	"github.com/bailaohe/goblog/accountservice/dbclient"
 )
 
 var appName = "accountservice"
 
 func main() {
 	fmt.Printf("Starting %v\n", appName)
+	initializeBoltClient()
 	service.StartWebServer("6767")
+}
+
+func initializeBoltClient() {
+	service.DBClient = &dbclient.BoltClient{}
+	service.DBClient.OpenBoltDb()
+	service.DBClient.Seed()
 }
--- accountservice/service/routes.go
+++ accountservice/service/routes.go
@@ -21,9 +21,18 @@ var routes = Routes{
 		"GetAccount",                                     // Name
 		"GET",                                            // HTTP method
 		"/accounts/{accountId}",                          // Route pattern
-		func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-			w.Write([]byte("{\"result\":\"OK\"}"))
-		},
+		GetAccount,
+	},
+	Route{
+		"HealthCheck",
+		"GET",
+		"/health",
+		HealthCheck,
+	},
+	Route{
+		"Testability",
+		"GET",
+		"/testability/healthy/{state}",
+		SetHealthyState,
 	},
 }
